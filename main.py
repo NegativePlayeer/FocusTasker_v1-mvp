0f345801dc8b60905347e0b22e529f87e0780479
NegativePlayeer <108997804+NegativePlayeer@users.noreply.github.com>
2026-03-28 13:02:57 +0100

    Connecting database file and models with main file

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from fastapi import FastAPI
+from database import engine, Base, SessionLocal
+from Models import task, user
 app = FastAPI()
+Base.metadata.create_all(bind=engine)
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
