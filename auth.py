cdb0355555c89cc5ba20fb7dcf68b53dd57efd31
NegativePlayeer <108997804+NegativePlayeer@users.noreply.github.com>
2026-03-31 23:34:33 +0200

    Creating access token function with JWT

--- auth.py
+++ auth.py
@@ -1,6 +1,7 @@
 import bcrypt
-import jose
+from jose import jwt
 from datetime import datetime, timedelta, timezone
+from config import settings
 
 def hash_password(password: str) -> str:
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
@@ -11,4 +12,9 @@ def verify_password(password: str, hashed_password: str) -> bool:
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password.encode('utf-8'))
 
 def create_access_token(data: dict) -> str:
-    pass
+    payload = data.copy()
+
+    expires = datetime.now(timezone.utc) + timedelta(minutes=30)
+    payload.update({"exp": expires})
+
+    return jwt.encode(payload, key=settings.SECRET_API_KEY, algorithm='HS256')
