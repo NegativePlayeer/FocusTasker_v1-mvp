fc2bae91879eab251a47dca67d4a2b16f1630aca
NegativePlayeer <108997804+NegativePlayeer@users.noreply.github.com>
2026-04-01 21:32:38 +0200

    return JWT token after valid authentication

--- routers/auth.py
+++ routers/auth.py
@@ -14,3 +14,6 @@ async def authenticate_user(
     user_model = db.query(User).filter(User.username == user_data.username).first()
     if user_model is None or not auth.verify_password(user_data.password, user_model.hashed_password):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Authentication failed!')
+
+    token = auth.create_access_token({'sub': user_model.username, 'id': user_model.id})
+    return {'access_token': token, 'token_type': 'bearer'}
