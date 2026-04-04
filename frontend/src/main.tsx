5fbb79482a031462fa6ef6de28888036fde51ad7
NegativePlayeer <108997804+NegativePlayeer@users.noreply.github.com>
2026-04-04 11:45:58 +0200

    Routing to dashboard

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import LoginPage from "@/pages/LoginPage.tsx";
+import Dashboard from "@/pages/Dashboard.tsx"
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+      <Routes>
+          <Route path='/' element={<Navigate to='/login' />} />
+          <Route path='/login' element={<LoginPage />}/>
+          <Route path='/dashboard' element={<Dashboard />}/>
+      </Routes>
+  </BrowserRouter>
 )
---
+++ frontend/src/pages/Dashboard.tsx
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+
+
+function Dashboard() {
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    )
+}
+
+export default Dashboard
