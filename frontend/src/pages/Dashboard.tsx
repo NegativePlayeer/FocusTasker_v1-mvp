0b480cd32f19b19b21a9c6867f2542c6a514e5d6
NegativePlayeer <108997804+NegativePlayeer@users.noreply.github.com>
2026-04-04 12:52:21 +0200

    dashboard with tasks list

--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,15 +1,36 @@
+import {useEffect, useState} from "react";
 
-
-
-
-
-
+interface Task {
+    id: number
+    title: string
+    description: string
+    priority: number
+    is_completed: boolean
+}
 
 
 function Dashboard() {
+    const [tasks, setTasks] = useState<Task[]>([])
+    const listTasks = tasks.map(task => <li key={task.id}>{task.title}</li>)
+
+    useEffect(() => {
+        const fetchTasks = async () => {
+            const token = localStorage.getItem('token')
+            const response = await fetch('http://localhost:8000/tasks/', {
+                method: 'GET',
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                }
+            })
+            const data = await response.json()
+            setTasks(data)
+        }
+        fetchTasks()
+    }, [])
+
     return (
         <div>
-            <h1>Hello</h1>
+            <ul>{listTasks}</ul>
         </div>
     )
 }
