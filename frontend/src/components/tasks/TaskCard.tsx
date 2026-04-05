fe60057e8797cc047b374be01abb024d2fe0d9a1
NegativePlayeer <108997804+NegativePlayeer@users.noreply.github.com>
2026-04-05 22:56:35 +0200

    task card with motion

--- frontend/src/components/tasks/TaskCard.tsx
+++ frontend/src/components/tasks/TaskCard.tsx
@@ -1,1 +1,31 @@
-import task
+import type { Task } from "@/types/task.ts";
+import { motion } from "framer-motion";
+
+interface TaskCardProps {
+    task: Task,
+    onClick: () => void
+}
+
+const priorityLabel: Record<number, string> = {
+    1: 'Do it right now!',
+    2: 'Eye catching, but...',
+    3: 'Well... Do it later'
+}
+
+function TaskCard({task, onClick}: TaskCardProps) {
+
+    return (
+        <motion.div
+            whileHover={{scale: 1.02}}
+            whileTap={{scale:.98}}
+            onClick={onClick}
+            className={`bg-card border border-border border-l-4 ${priorityLabel[task.priority]} rounded-xl p-4 cursor-pointer shadow-sm`}
+        >
+            <h3 className="font-semibold text-foreground text-base mb-1">{task.title}</h3>
+            <p className="text-muted-foreground text-sm line-clamp-2">{task.description}</p>
+            <span className="text-xs mt-2 inline-block">{priorityLabel[task.priority]}</span>
+        </motion.div>
+    )
+}
+
+export default TaskCard
