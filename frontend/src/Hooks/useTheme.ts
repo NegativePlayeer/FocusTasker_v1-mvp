8750f446457eb13c20507216a4a1679a9caf809a
NegativePlayeer <108997804+NegativePlayeer@users.noreply.github.com>
2026-04-05 22:38:42 +0200

    design for navbar

---
+++ frontend/src/Hooks/useTheme.ts
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react";
+
+
+export function UseTheme(){
+    const [isDark, setIsDark] = useState<boolean>(false)
+
+    useEffect(() => {
+        const saved = localStorage.getItem('theme')
+        if(saved === 'dark'){
+            document.documentElement.classList.add('dark')
+            setIsDark(true)
+        }
+    }, [])
+
+    const toggle = () => {
+        if(isDark) {
+            document.documentElement.classList.remove('dark')
+            localStorage.setItem('theme', 'light')
+        } else {
+            document.documentElement.classList.add('dark')
+            localStorage.setItem('theme', 'dark')
+        }
+        setIsDark(prevState => !prevState)
+    }
+    return {isDark, toggle}
+}
