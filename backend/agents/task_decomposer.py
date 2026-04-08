316416e670252bcdcb5414bd61a5a96220d8bdfa
NegativePlayeer <108997804+NegativePlayeer@users.noreply.github.com>
2026-04-08 21:23:55 +0200

    feat: add preferences and struggles fields to User model and personalize decompose promp with the user preferences

--- backend/agents/task_decomposer.py
+++ backend/agents/task_decomposer.py
@@ -1,24 +1,30 @@
 import anthropic
 import sys
 import os
+
+from schemas.user import UserResponse
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from config import settings
 import json
 import re
 
-def decompose_task(title: str):
+def decompose_task(title: str, user: UserResponse):
     client = anthropic.Anthropic(api_key=settings.ANTHROPIC_KEY)
     response = client.messages.create(
         model='claude-haiku-4-5-20251001',
         max_tokens=1024,
-        system="""You are an expert/coach in adhd, your goal is to decompose my task into smaller parts to start easier. I am a student who has adhd and needs this decomposition to get ready with the task as fast as possible. Tasks should last no longer than 10 minutes and contain only 5-7 steps and also title should not have more than 10 words.
+        system=f"""You are an expert/coach in adhd, your goal is to decompose my task into smaller parts to start easier. 
+         User preferences: {user.preferences}.
+         User struggles: {user.struggles}
+         Tasks should last no longer than 10 minutes and contain only 5-7 steps and also title should not have more than 10 words.
 
         Return ONLY the raw JSON, no markdown, no code blocks, no explanation in this format:
-        {
+        {{
           "steps": [
-            {"step": 1, "title": "...", "duration_minutes": 5}
+           {{"step": 1, "title": "...", "duration_minutes": 5}}
           ]
-        }
+        }}
         """,
         messages=[
             {'role': 'user', 'content': f'decompose task: {title}'}
--- backend/models/user.py
+++ backend/models/user.py
@@ -14,4 +14,6 @@ class User(Base):
     hashed_password = Column(String)
     role = Column(String)
     is_active = Column(Boolean, default=True)
+    preferences = Column(String)
+    struggles = Column(String)
     tasks = relationship('Task', back_populates='owner')
